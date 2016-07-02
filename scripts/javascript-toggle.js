c4c3e5e7936cd65fdb7123b0e469bba9085ea053
Michael Szeto <szetomj@gmail.com>
2016-07-02 23:21:00 +1200

    fixed code to effect all classes not one specific one

--- scripts/javascript-toggle.js
+++ scripts/javascript-toggle.js
@@ -1,9 +1,15 @@
-var div = document.querySelectorAll('div.div-default');
+var divs = document.querySelectorAll('div.div-default'); //queryslectorall lists all the elements you are looking for on the DOM.
 
-for(var x = 0; x < div.length; x++){
-	div[1].addEventListener('click', toggleClass, false);
-}
+for(var x = 0; x < divs.length; x++) {
+	divs[x].addEventListener('click', toggleClassDanger, false);
+} //the for loop will run through the dom tree for all the divs with the class btn.
+  //False will say to not stop at the first class it finds. It will go all the way to the bottom of the DOM first. Then once a click event is fired, go up the tree to the one element that was clicked 
 
-function toggleClass(e){
-	e.currentTarget.classList.toggle('div-danger');
+function toggleClassDanger(e){
+	e.currentTarget.classList.toggle('div-danger'); //This will toggle the div-danger class
+
+	//e.currentTarget.classList.add('btn-danger'); //This will add the class 'btn-danger'
+	//e.currentTarget.classList.remove('btn'); //This will remove the class 'btn'
+
+	//e.currentTarget.setAttribute('class', 'btn-danger'); //This will remove all the other classes that the target has and replace them all with the one class 'btn-danger'
 }
